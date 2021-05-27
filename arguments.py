e8eb867cc9032e31f4b00199772f01189b0a0500
nina-chitkina <nin.chitashvili@gmail.com>
2021-05-27 14:18:33 +0200

    added more arguments in python file

--- arguments.py
+++ arguments.py
@@ -1,15 +1,28 @@
 import argparse
-
-
+import datetime
+import os
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--name', type=str, default='friend', 
+parser.add_argument('--name', type=str, 
                         help='says Hi and your name')
-parser.add_argument('--lastname', type=str, default='friendson', 
-                        help='says Hi and your lastname')           
+parser.add_argument('--lastname', type=str, 
+                        help='says Hi and your lastname')  
+parser.add_argument('--date', action='store_true',
+			 help='shows todays date')
+                        
+                                 
 args = parser.parse_args()
 
 name = args.name
 lastname = args.lastname
 
-print(f"Hi, {name[:1]}. {lastname}!")
+if args.name and args.lastname:
+    print(f"Hi, {name[:1]}. {lastname}!")
+elif args.name:
+    print(f"Hi, {name}!")
+elif args.lastname:
+    print(f"Hi, {lastname}!")
+if args.date:
+    print(f"Today is {datetime.datetime.now()}")
+
+
